e39e4af8cec4e47faf23ae5fb718b8ce3d2a770a
prashanth kumar <32808287+pacchi1997@users.noreply.github.com>
2021-09-15 10:06:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import SimpleInput from './components/SimpleInput';
+//import SimpleInput from './components/SimpleInput';
+//import SimpleInputCustomHook from './components/SimpleInputCustomHook';
+
+import BasicForm from "./components/BasicForm";
 
 function App() {
+
   return (
     <div className="app">
-      <SimpleInput />
+     
+  <BasicForm />
+   
+    
     </div>
   );
 }
